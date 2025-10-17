89eb502c553768b9f3a68deafda3312a861fb270
Nikhil Kachhela <kachhela.n@northeastern.edu>
2025-10-17 18:50:37 -0400

    Removed main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Member1.main(args);
-        Member2.main(args);
-        Member3.main(args);
-        Member4.main(args);
-    }
-}
-
